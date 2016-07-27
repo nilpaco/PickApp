6c2357b821482415ec952f74dfa83dc9c371dcf8
Nil Panés <nil.panes@gmail.com>
2016-07-27 21:28:52 +0200

    0.0.1

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pickAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pickAppApp
+ */
+angular.module('pickAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pickAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pickAppApp
+ */
+angular.module('pickAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
