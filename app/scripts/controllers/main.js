87e3274c92cb704be20d9566db89b83d07fe96b0
Nil Panés <nil.panes@gmail.com>
2016-08-03 20:21:50 +0200

    Some html addons

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,6 +9,27 @@
  */
 angular.module('pickAppApp')
   .controller('MainCtrl', function ($scope) {
+    $scope.showFirst = true;
+    $scope.showSecond = false;
+    $scope.showThird = false;
+
+    $scope.changeView = function(val){
+      if(val == 'first'){
+        $scope.showFirst = true;
+        $scope.showSecond = false;
+        $scope.showThird = false;
+      } else if (val == 'second') {
+        $scope.showFirst = false;
+        $scope.showSecond = true;
+        $scope.showThird = false;
+      } else if (val == 'third') {
+        $scope.showFirst = false;
+        $scope.showSecond = false;
+        $scope.showThird = true;
+      }
+
+    }
+
     $scope. cards=[];
     $scope.cards = [
             { title: "Cards 1",
