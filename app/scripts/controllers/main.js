0b3a8709f41fdfd6764f9c6d826688ca4dac1245
Nil Panés <nil.panes@gmail.com>
2016-08-03 16:39:33 +0200

    Some html addons

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,19 @@
  * Controller of the pickAppApp
  */
 angular.module('pickAppApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope) {
+    $scope. cards=[];
+    $scope.cards = [
+            { title: "Cards 1",
+              user: "username",
+              location: "Vallcarca",
+              img: "http://placehold.it/350x150"
+            },
+            {title: "Cards 1",
+              user: "username",
+              location: "Vallcarca",
+              img: "http://placehold.it/350x150"}
+        ];
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
